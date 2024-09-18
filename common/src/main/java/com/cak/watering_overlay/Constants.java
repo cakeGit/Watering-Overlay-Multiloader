7b1a46f61aef67bc86d0a67a89a6dc168a2f931a
cakeGit <cakeistastydev@gmail.com>
2024-09-18 16:28:57 +0100

    Port begin

---
+++ common/src/main/java/com/cak/watering_overlay/Constants.java
@@ -0,0 +1,11 @@
+package com.cak.watering_overlay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "watering_overlay";
+	public static final String MOD_NAME = "Watering Overlay";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/com/cak/watering_overlay/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.cak.watering_overlay.platform;
+
+import com.cak.watering_overlay.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
